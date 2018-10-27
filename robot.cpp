8395e42c896936b4693999c8bc384d97c82cf3f0
Cameron Graybill <csg58@drexel.edu>
2018-10-27 10:50:11 -0400

    initial commit

---
+++ robot.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "WE ARE DART!" << std::endl;
+    std::cout << "AND WE HAVE CI" << std::endl;
+    std::cout << "AND WE HAVE LOCATION!" << std::endl;
+    return 0;
+}
